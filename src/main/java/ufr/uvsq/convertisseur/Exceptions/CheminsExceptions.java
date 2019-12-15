c50131130fcaad7941e9bb4e72f7318a0e4db687
RachData <rachidderra@gmail.com>
2019-12-15 12:35:28 +0000

    Mise a jour

---
+++ src/main/java/ufr/uvsq/convertisseur/Exceptions/CheminsExceptions.java
@@ -0,0 +1,7 @@
+package ufr.uvsq.convertisseur.Exceptions;
+
+public class CheminsExceptions extends Exception {
+	public CheminsExceptions(){
+		System.out.println("\nLe Fichier specifié n'existe pas\n");
+	}
+}
