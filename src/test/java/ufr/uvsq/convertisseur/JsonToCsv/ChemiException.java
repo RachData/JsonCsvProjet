0dc3a9af897cc7db69640a1b48ddbecd6f8128bb
RachData <rachidderra@gmail.com>
2019-12-16 16:57:33 +0100

    GESTION DES TESTS

--- src/test/java/ufr/uvsq/convertisseur/JsonToCsv/ChemiException.java
+++ src/test/java/ufr/uvsq/convertisseur/JsonToCsv/ChemiException.java
@@ -2,13 +2,37 @@ package ufr.uvsq.convertisseur.JsonToCsv;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class ChemiException {
+import ufr.uvsq.convertisseur.Exceptions.CheminsExceptions;
+import ufr.uvsq.convertisseur.Exceptions.FichiersVide;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class ChemiException {
+	String chemin1;
+	String chemin2;
+	String chemin3;
+	String chemin4;
+	Convertisseur convertir;
+	
+
+	@Before
+	  public void setup() {
+		String chemin1="yggkjh";
+		String chemin2="C:\\Users\\utilisateur\\Documents\\Java\\convertisseur\\JsonCsvProjet\\src\\main\\Ressources\\Csv_to_Json\\jsonFromCsv.json2";
+		String chemin3="C:\\Users\\utilisateur\\Documents\\Java\\convertisseur\\JsonCsvProjet\\src\\main\\Ressources\\Csv_to_Json\\videJson.json2";
+		String chemin4=null;
+		convertir =new Convertisseur();
+	  }
+
+	 @Test()
+	public void test() throws CheminsExceptions {
+		 try {
+			 convertir.to_csv(chemin3);
+		 }catch(Exception es) {
+			 
+		 }
+		 
 	}
 
 }
